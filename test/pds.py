09e76632c8fcbb1b0e6c2e82a8a7fb5c5889c8e9
Akkarapon Phikulsri <lacakira@gmail.com>
2022-09-07 19:09:59 +0700

    Update pds.py

--- test/pds.py
+++ test/pds.py
@@ -15,13 +15,6 @@ graph = [
          ['t','-->','w'],  
 ]
 
-f = []
-e = []
-d = 0
-s = 'u'
-g = 'w'
-count = 0
-
 def get_moves(start_state, g_list):
 	if(g_list == []): return []
 	else:
@@ -34,7 +27,8 @@ def get_moves(start_state, g_list):
 			return get_moves(start_state, temp_graph)
 
 def progressive_deeping_search(start, goal, graph):
-	return pds_q(0, [[start]], goal, [], [], graph)
+	res = pds_q([[start]], goal, [], [], graph)
+	return res
 
 def extend_path(path, next_frontier, explored):
 	if(len(next_frontier)<1): 
@@ -50,18 +44,17 @@ def extend_path(path, next_frontier, explored):
 		explored.append(el)
 		return [[el]+path] + extend_path(path, next_frontier, explored)
 
-def pds_q(depth, start, goal, frontier, explored, graph):
-	print(f"\n{depth}-- PDS path--\n", start)
-	if(len(start) < 1):
-		return start
-	elif start[0][0] == goal:
-		return start[0]
+def pds_q(paths, goal, frontier, explored, graph):
+	global depth
+	print("\n progressive deeping search \n path--> {} \n depth = {}\n".format(paths, depth))
+	current_node = paths[0][0]
+	if(len(paths) < 1):
+		return paths
+	elif current_node == goal:
+		return paths[0]
 	else:
-		temp_graph = graph.copy()
-		temp_paths = start.copy()
-		p = temp_paths[0]
-		x = temp_paths[0][0]
-		temp_paths.pop(0)
+		temp_graph, temp_paths = graph.copy(), paths.copy()
+		current_path, present_node = temp_paths[0], temp_paths[0][0]
 
 		for node in frontier:
 			if(node not in explored):
@@ -69,9 +62,11 @@ def pds_q(depth, start, goal, frontier, explored, graph):
 			frontier.pop(frontier.index(node))
 
 		if frontier == []: depth+=1
-		next_frontier = get_moves(x, temp_graph)
-		next_paths = extend_path(p, next_frontier, explored)
-		return pds_q(depth, next_paths + temp_paths, goal, frontier, explored, temp_graph)
+		next_frontier = get_moves(present_node, temp_graph)
+		next_paths = extend_path(current_path, next_frontier, explored)
+		return pds_q(next_paths + temp_paths, goal, frontier, explored, temp_graph)
 
-res = progressive_deeping_search(s, g, graph)
-print(res)
+depth=0
+pds = progressive_deeping_search('u', 'w', graph)
+
+print("\n=== result === \n path: {} \n depth = {}".format(pds, depth))
